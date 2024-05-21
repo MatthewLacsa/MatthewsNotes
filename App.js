bf7e173909099abf86b60d62fe3213d6c4f73ff0
Matthew Lacsa <matthewlacsa@gmail.com>
2024-05-21 18:30:25 -0300

    first try

--- App.js
+++ App.js
@@ -1,21 +1,33 @@
-import { SafeAreaView, Text } from 'react-native';
+import { SafeAreaView, Text, FlatList, View } from 'react-native';
 import tw, { useDeviceContext } from 'twrnc';
 import { Provider } from 'react-redux';
 import { store } from './store';
 import 'react-native-reanimated'; 
 
-function App() {
-  useDeviceContext(tw);
+const generateData = (count) => Array.from({ length: count }, (_, i) => ({ id: (i + 1).toString() }));
 
+const data = generateData(20)
+
+const Card = ({ item }) => {
   return (
-    <Provider store={store}>
-      <SafeAreaView>
-        <Text style={tw`w-screen mt-16 text-center text-xl`}>
-          Your app code goes here.
-        </Text>
-      </SafeAreaView>
-    </Provider>
-  )
-}
+    <View style={[tw`w-full h-12 mb-1 mr-1 rounded-lg p-4 bg-purple-400`]}>
+      <Text>{ item.id }</Text>
+    </View>
+  );
+};
+
+const App = () => {
+  return (
+    <FlatList
+      data={data}
+      keyExtractor={(item) => item.id}
+      renderItem={({ item }) => <Card item={item} />}
+      numColumns={1}
+      contentContainerStyle={tw`p-4`}
+    />
+  );
+};
 
 export default App;
+
+
