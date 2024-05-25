7d6dfe348aac178d97fc09df3763c7350754e748
Matthew Lacsa <matthewlacsa@gmail.com>
2024-05-24 23:06:31 -0300

    testing to see if the notes can be edited and saved, visuals will do later, and im planning to do the delete feature next

--- App.js
+++ App.js
@@ -1,13 +1,14 @@
-import {useEffect, useLayoutEffect} from 'react';
+import {useState, useEffect, useLayoutEffect} from 'react';
 import { SafeAreaView, Text, FlatList, View, TextInput, TouchableOpacity, Button } from 'react-native';
 import tw, { useDeviceContext } from 'twrnc';
 import { Provider } from 'react-redux';
 import { store } from './store';
 import 'react-native-reanimated'; 
 import MasonryList from '@react-native-seoul/masonry-list'
-import { useSearchNotesQuery, useAddNoteMutation, useDeleteNoteMutation } from './db';
+import { useSearchNotesQuery, useAddNoteMutation, useDeleteNoteMutation, useUpdateNoteMutation } from './db';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { NavigationContainer } from '@react-navigation/native';
+
 function MainNotes( {navigation} ) {
   const {data: searchData, error, isLoading} = useSearchNotesQuery("");
   const [addNote, { data: addNoteData, error: addNoteError}] = useAddNoteMutation();
@@ -15,14 +16,13 @@ function MainNotes( {navigation} ) {
 
   useEffect(() => {
     if(addNoteData != undefined) {
-      console.log(addNoteData.title);
       navigation.navigate("Edit", {data: addNoteData});
     }
 
   }, [addNoteData]);
 
   const renderItem = ({ item }) => (
-    <TouchableOpacity onPress = {() => deleteNote(item) } style = {tw `w-[98%] mb-0.5 mx-auto bg-red-300 rounded-lg px-1`}>
+    <TouchableOpacity onPress = {() => navigation.navigate("Edit", { data: item }) } style = {tw `w-[98%] mb-0.5 mx-auto bg-red-300 rounded-lg px-1`}>
       <Text>{item.title} {item.id}</Text>
     </TouchableOpacity>
   )
@@ -40,7 +40,7 @@ function MainNotes( {navigation} ) {
       : <></>
     }
       <View style={tw`flex-row items-center`}>
-        <TouchableOpacity style = {tw`bg-red-100 p-2 m-6 rounded-lg `} onPress = {() => {addNote({title: "test", content: "content"})}}>
+        <TouchableOpacity style = {tw`bg-red-100 p-2 m-6 rounded-lg `} onPress = {() => {addNote({title: "", content: ""})}}>
         <Text style = {tw`text-sm font-bold text-center `}>Add Note</Text>
         </TouchableOpacity>
         <TextInput placeholder="Search for a note" style={tw`h-8 p-2 bg-red-100 rounded-lg m-4 text-sm w-50`} />
@@ -49,29 +49,36 @@ function MainNotes( {navigation} ) {
   );
 }
 function EditScreen({ route, navigation }) {
+  const[title, setTitle] = useState(route.params.data.title);
+  const[content, setContent] = useState(route.params.data.content)
+  const[saveNote, {}] = useUpdateNoteMutation(); 
   useLayoutEffect(() => {
     navigation.setOptions({ title: route.params.data.title });
   }, []);
 
   return (
-    <View style={tw`flex-1 items-center justify-center bg-red-400`}>
-      <Text style={tw`text-lg text-white`}>Edit Screen {route.params.data.title} {route.params.data.id}</Text>
+    <View style={tw`flex-1 bg-red-400 p-2 items-center justify-center`}>
+      <TextInput placeholder = "Please enter title" style = {tw`flex-1 bg-white p-2`} value= {title} onChangeText={setTitle}/>
+      <TextInput placeholder = "Please enter text" style = {tw`flex-1 bg-white p-2`} value= {content} onChangeText={setContent}/>
+      <TouchableOpacity style = {tw`bg-red-100 p-2 m-6 rounded-lg `} onPress = {() => saveNote({id: route.params.data.id, title, content,})} ></TouchableOpacity>
     </View>
   );
 }
 
 const Stack = createNativeStackNavigator();
+
 export default function MyNotes() {
   return (
-    <Provider store={store}>
+    //prop store added
+    <Provider store={store}> 
       <NavigationContainer>
-        <Stack.Navigator initialRouteName="Home">
+        <Stack.Navigator initialRouteName="Main">
           <Stack.Screen
             options={{
               headerStyle: tw`bg-red-300 border-0`,
               headerTintColor: '#fff',
-              headerTitleStyle: tw`font-bold`,
-              headerShadowVisible: false, // gets rid of border on device
+              headerTitleStyle: tw`font-bold`, 
+              headerShadowVisible: false, 
             }}
             name="Note App"
             component={MainNotes}
@@ -81,7 +88,7 @@ export default function MyNotes() {
               headerStyle: tw`bg-red-300 border-0`,
               headerTintColor: '#fff',
               headerTitleStyle: tw`font-bold`,
-              headerShadowVisible: false, // gets rid of border on device
+              headerShadowVisible: false,
             }}
             name="Edit"
             component={EditScreen}
